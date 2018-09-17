8c1ab4142f6ca21ae124b0b38a34072b96556638
Bharadwaj Ramakrishnan <bharadwaj.r@caratlane.com>
2018-09-18 02:43:15 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './Login';
+import 'styles/app.scss';
+
+class App extends React.Component {
+  render() {
+    return <div>
+      <Login />
+    </div>;
+  }
+}
+
+export default App;
