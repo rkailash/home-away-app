afc083de055b66c2f46604c3bc26c0b66e3886cd
Kailash Ramakrishnan <kailash.ramakrishna@gmail.com>
2018-09-29 10:05:34 -0700

    Update Details.js

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React from "react";
 
 class Details extends React.Component {
   render() {
     return (
-      <div className="details">Details</div>
-    )
+      <div className="panel panel-default">
+        <div className="panel-body">
+          <h2>Describe your property</h2>
+          <hr />
+          <form className="details-form">
+            <div className="form-group">
+              <label>Headline</label>
+              <input id="headline" type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+              <label>Property description</label>
+              <input id="pdescription" type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+              <label>Property type</label>
+              <input id="ptype" type="text" className="form-control" />
+            </div>
+
+            <div className="form-group">
+              <label>Bedrooms</label>
+              <input id="bedrooms" type="text" className="form-control" />
+            </div>
+
+            <div className="form-group">
+              <label>Accomodates</label>
+              <input id="accomodates" type="text" className="form-control" />
+            </div>
+
+            <div className="form-group">
+              <label>Bathrooms</label>
+              <input id="bathrooms" type="text" className="form-control" />
+            </div>
+            <hr />
+            <button className="btn btn-primary">Cancel</button>
+            <button className="btn btn-primary">Save</button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
