a902dff3c1754262b2ae9c1a92663785b7ad8331
Kailash Ramakrishnan <kailash.ramakrishna@gmail.com>
2018-09-29 09:40:29 -0700

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,28 +1,57 @@
 import React, {Component} from 'react';
-import Select from 'react-select';
+import {Link} from 'react-router-dom';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import 'styles/header.scss';
 
-const options = [
-  {value: 'traveler', label: 'Traveler Login'},
-  {value: 'owner', label: 'Owner Login'},
-];
+// const options = [
+//   {value: 'traveler', label: 'Traveler Login'},
+//   {value: 'owner', label: 'Owner Login'},
+// ];
 
-const menuStyles = {
-  control: styles => ({ ...styles, width: '100px'}),
-  container: styles => ({ ...styles, width: '200px'}),
-};
+// const menuStyles = {
+//   control: styles => ({
+//     ...styles,
+//     width: '100px',
+//     backgroundColor: 'transparent',
+//     height: '100%',
+//     border: 0
+//   }),
+//   container: styles => ({ ...styles, width: '200px'}),
+//   placeholder: styles => ({...styles, color: 'white', fontFamily: 'inherit'}),
+//   dropdownIndicator: styles => ({...styles, color: 'white'}),
+//   indicatorSeparator: styles => ({...styles, display: 'none'})
+// };
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dropdownOpen: false
+    }
+  }
   onClickLogin = (option) => {
     this.props.onClick(option.value)
   };
+  toggle = () => {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
   render() {
-    const {showLogin} = this.props;
+    const {showLogin, onClick, design} = this.props;
     return (
-      <div className="header">
+      <div className={`header ${design === 'gradient' ? 'gradient' : ''}`}>
         <img className="logo" src="https://csvcus.homeaway.com/rsrcs/cdn-logos/2.11.0/bce/moniker/homeaway_us/logo-bceheader-white.svg" />
         {showLogin &&
-          <Select onChange={(e) => this.onClickLogin(e)} styles={menuStyles} placeholder={'Login'} options={options} />
+          <Dropdown className="header-menu" isOpen={this.state.dropdownOpen} toggle={() => this.toggle()}>
+            <DropdownToggle caret>
+              Login
+            </DropdownToggle>
+            <DropdownMenu>
+            <DropdownItem><Link to="/login">Traveler Login</Link></DropdownItem>
+              <DropdownItem><Link to="/login">Owner Login</Link></DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         }
       </div>
     );
