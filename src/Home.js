7b4ed9c88170790722f257d1a976faebf615d12b
Bharadwaj Ramakrishnan <bharadwaj.r@caratlane.com>
2018-10-02 12:30:01 +0530

    Changes

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,31 @@
 import React from "react";
 import Search from "./Search";
+import RecentActivity from './RecentActivity'
+import Header from './Header';
 import "styles/home.scss";
 
 const Home = () => (
   <div className="home">
     <div className="hero-container">
-      <div className="padding-container">
-        <Search />
-      </div>
+      <Header design="gradient" showLogin />
+      <h1>Book beach houses, cabins,<br></br>condos and more, worldwide.</h1>
+      <Search />
+      <ul className="message-container">
+        <li>
+          <h4>Your whole vacation starts here</h4>
+          <small>Choose a rental from the world's best selection.</small>
+        </li>
+        <li>
+          <h4>Book and stay with confidence</h4>
+          <small>Secure payments, peace of mind</small>
+        </li>
+        <li>
+          <h4>Your vacation your way</h4>
+          <small>More space, more privacy, no compromises</small>
+        </li>
+      </ul>
     </div>
+    <RecentActivity />
   </div>
 );
 
