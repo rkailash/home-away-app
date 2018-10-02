7b65b158b9156abfa7f0343e86a1d0e65d8c0f81
kailash <kailash.ramakrishna@gmail.com>
2018-10-01 23:46:29 -0700

    latest update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+=======
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+>>>>>>> Stashed changes
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root"));
+  document.getElementById("root")
+);
